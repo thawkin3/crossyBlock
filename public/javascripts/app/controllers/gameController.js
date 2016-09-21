9d6be66056d91530b22ad95df19836a219736b58
Tyler Hawkins <thawkin3@byu.net>
2016-09-20 23:05:12 -0600

    finished v1 of crossy block

--- public/javascripts/app/controllers/gameController.js
+++ public/javascripts/app/controllers/gameController.js
@@ -1,57 +1,54 @@
 (function() {
 
-	var gameController = function ($scope, $routeParams, $rootScope, $location, $timeout) {
+	var gameController = function ($scope, $routeParams, $rootScope, $location, $timeout, $interval) {
+
+		///////////////////////////////////////////////
+		/// VARIABLES
+		///////////////////////////////////////////////
 
 		// variables for game play
+		var gameStart = false;
 		var gameEnd = false;
-		var gameWin = false;
+		$scope.score = 0;
 
-		// variables for the ball
-		var ball_size = 10;
-		var ball_x = 220;
-		var ball_y = 220;
-		var ball_dx = 3;
-		var ball_dy = 3;
-
-		// variables for the paddle
-		var paddle_width = 100;
-		var paddle_height = 10;
-		var paddle_x = 330;
-		var paddle_y = 450;
-		var paddle_dx = 4;
+		// variables for your player
+		var player_width = 10;
+		var player_height = 10;
+		var player_x = 200;
+		var player_y = 440;
+		var player_dx = 2;
+		var player_dy = 2;
 		var left = false;
 		var right = false;
-		var hitReset = true;
-		var theCount = 0;
+		var up = false;
+		var down = false;
 
-		// variables for the bricks
-		$scope.brickArray = [];
-		var brick_height = 20;
-		var brick_width = 40;
-		var brick_top = 10;
-		var brick_left = 10;
-		
-		for (var i = 0; i < 32; i++) {
-			if (i != 0) {
-				brick_left += (brick_width + 10);
-			}
-			if (i == 8 || i == 16 || i == 24) {
-				brick_top += 30;
-				brick_left = 10;
-			}
-			$scope.brickArray.push({ h: brick_height, w: brick_width, t: brick_top, l: brick_left });
-		}
+		// variables for the obstacles
+		var obstacles = [];
 
-		var filterBricksHitFromBelow = function(brick) {
-			return ((ball_y - brick.t > 35 || ball_y - brick.t < 10) || (ball_x - brick.l < 0 || ball_x - brick.l > 40));
-		}
+		// need the same kind of obstacle in each row, spaced evenly
+		// small cars, long cars, trains
+		// slow, medium, fast
+		// spaced horizontally differently per row
+		// moving in different directions per row
+		// spaced vertically differently between rows
+		var obstacleSettings = {
+			top: 300,
+			left: -10000,
+			height: 10,
+			size: [30, 60, 90, 120, 150, 180],
+			speed: [0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4],
+			direction: [-1, 1],
+			spacing_horizontal: [40, 60, 90, 120, 150, 180],
+			spacing_vertical: [15, 20, 30, 40, 50, 60]
+		};
 
-		var filterBricksHitFromAbove = function(brick) {
-			return ((brick.t - ball_y > 10 || brick.t - ball_y < -15) || (ball_x - brick.l < 0 || ball_x - brick.l > 40));
-		}
+		///////////////////////////////////////////////
+		/// FUNCTIONS
+		///////////////////////////////////////////////
 
 		// Submit your score
-		var submitScore = function(){
+		$scope.submitScore = function() {
 			var scoreObj = { "Username": $rootScope.user, "Score": $scope.score };
 	        // console.log(scoreObj);
 	        var JSONscoreObj = JSON.stringify(scoreObj);
@@ -76,212 +73,78 @@
 				$location.url("/highscores");
 			}, 2000);
 		};
-		
-		// keep score
-		$scope.scoreHelper = 0;
-		$scope.score = 0;
-
-		// set up the canvas
-		var canvas = document.getElementById("gameCanvas");
-		var ctx = canvas.getContext("2d");
-
-		// $scope.goToScores = function() {
-		// 	console.log("going to high scores 1");
-		// 	gameEnd = true;
-		// 	$timeout(function() {
-		// 		$location.url("/highscores");
-		// 	}, 100);
-		// }
 
 		// Locator function in a loop
 		$scope.mainLoop = function() {			
 			// logic for the game to end or not
 			if (!gameEnd) {
-				// draw our canvas here
-				// console.log(ball_y);
+				// draw and update our canvas here
 				$scope.draw();
 				$scope.update();
 				// Recursively call our loop
 				window.requestAnimationFrame($scope.mainLoop);
 			} else {
+				// Stop incrementing your score
+				$interval.cancel($scope.updateScore);
+
+				// Clear the canvas
+				ctx.fillStyle = "#050505";
+				ctx.fillRect(0, 0, canvas.width, canvas.height);
+				
 				// set the text that displays on the canvas
-				if (gameWin) {
-					ctx.font="20px zig";
-					ctx.fillText("YOU WIN",145,200);
-				} else {
-					ctx.font="20px zig";
-					ctx.fillText("GAME OVER",130,280);
-				}
-				// set our new score into the database
-				submitScore();
+				ctx.fillStyle = "#f1f1f1";
+				ctx.font = "20px zig";
+				ctx.fillText("GAME OVER",130,240);
+				
+				// add our new score into the database
+				$scope.submitScore();
 			}
 		};
 
 		// Update ball position
 		$scope.update = function() {
-			// Move the paddle, left key takes priority over right
-			if (left && paddle_x > 0 + paddle_width/2) {
-				paddle_x -= paddle_dx;
-			} else if (right && paddle_x < canvas.width - paddle_width/2) {
-				paddle_x += paddle_dx;
+			// Move the player, left key takes priority over right
+			if (left && player_x > 0 + player_width/2) {
+				player_x -= player_dx;
+			} else if (right && player_x < canvas.width - player_width/2) {
+				player_x += player_dx;
+			};
+			
+			// Move the player, up key takes priority over down.
+			if (up && player_y > -5 + player_height/2) {
+				player_y -= player_dy;
+			} else if (down && (player_y < canvas.height - player_height/2 || player_y > -5 + player_height/2)) {
+				player_y += player_dy;
 			};
 
-			// If the ball has hit the paddle, bounce it.
-		    if (ctx.getImageData(ball_x, ball_y + 1 + ball_size/2, 1, 1).data[0] == 242) {
-		    	ball_dy = -ball_dy;
-		    	if (left) {
-		    		if (ball_dx < 0) {
-		    			ball_dx -= 1;
-		    		} else {
-		    			ball_dx = -ball_dx;
-		    		}
-		    	}
-		    	if (right) {
-		    		if (ball_dx > 0) {
-		    			ball_dx += 1;
-		    		} else {
-		    			ball_dx = -ball_dx;
-		    		}
-		    	}
-		    }
-
-		    // If the ball has hit a brick from below, bounce it.
-		    if (ctx.getImageData(ball_x, ball_y - 3 - ball_size/2, 1, 1).data[0] == 241) {
-		    	ball_dy = -ball_dy;
-		    	$scope.$apply(function() {
-		    		$scope.brickArray = $scope.brickArray.filter(filterBricksHitFromBelow);
-					$scope.score = 32 + $scope.scoreHelper - $scope.brickArray.length;		    		
-					if ($scope.score == 32) {
-			    		$timeout(function(){	
-			    			$scope.scoreHelper = 48;
-			    			$scope.brickArray = [];
-							var brick_height = 20;
-							var brick_width = 40;
-							var brick_top = 10;
-							var brick_left = 10;
-							
-							for (var i = 0; i < 48; i++) {
-								if (i != 0) {
-									brick_left += (brick_width + 10);
-								}
-								if (i == 8 || i == 16 || i == 24 || i == 32 || i == 40) {
-									brick_top += 30;
-									brick_left = 10;
-								}
-								$scope.brickArray.push({ h: brick_height, w: brick_width, t: brick_top, l: brick_left });
-							}
-						}, 3000);
-		    		}
-		    		if ($scope.score == 80) {
-		    			$timeout(function(){
-			    			$scope.scoreHelper = 112;
-			    			$scope.brickArray = [];
-							var brick_height = 20;
-							var brick_width = 40;
-							var brick_top = 10;
-							var brick_left = 10;
-							
-							for (var i = 0; i < 64; i++) {
-								if (i != 0) {
-									brick_left += (brick_width + 10);
-								}
-								if (i == 8 || i == 16 || i == 24 || i == 32 || i == 40 || i == 48 || i == 56) {
-									brick_top += 30;
-									brick_left = 10;
-								}
-								$scope.brickArray.push({ h: brick_height, w: brick_width, t: brick_top, l: brick_left });
-							}
-						}, 3000);
-		    		}
-		    		if ($scope.score == 144) {
-		    			gameEnd = true;
-		    			gameWin = true;
-		    		}
-		    	});
-		    }
-
-		    // If the ball has hit a brick from above, bounce it.
-		    if (ctx.getImageData(ball_x, ball_y + 1 + ball_size/2, 1, 1).data[0] == 241) {
-		    	ball_dy = -ball_dy;
-		    	$scope.$apply(function() {
-		    		$scope.brickArray = $scope.brickArray.filter(filterBricksHitFromAbove);
-		    		$scope.score = 32 + $scope.scoreHelper - $scope.brickArray.length;
-		    		if ($scope.score == 32) {
-			    		$timeout(function(){	
-			    			$scope.scoreHelper = 48;
-			    			$scope.brickArray = [];
-							var brick_height = 20;
-							var brick_width = 40;
-							var brick_top = 10;
-							var brick_left = 10;
-							
-							for (var i = 0; i < 48; i++) {
-								if (i != 0) {
-									brick_left += (brick_width + 10);
-								}
-								if (i == 8 || i == 16 || i == 24 || i == 32 || i == 40) {
-									brick_top += 30;
-									brick_left = 10;
-								}
-								$scope.brickArray.push({ h: brick_height, w: brick_width, t: brick_top, l: brick_left });
-							}
-						}, 3000);
-		    		}
-		    		if ($scope.score == 80) {
-		    			$timeout(function(){
-			    			$scope.scoreHelper = 112;
-			    			$scope.brickArray = [];
-							var brick_height = 20;
-							var brick_width = 40;
-							var brick_top = 10;
-							var brick_left = 10;
-							
-							for (var i = 0; i < 64; i++) {
-								if (i != 0) {
-									brick_left += (brick_width + 10);
-								}
-								if (i == 8 || i == 16 || i == 24 || i == 32 || i == 40 || i == 48 || i == 56) {
-									brick_top += 30;
-									brick_left = 10;
-								}
-								$scope.brickArray.push({ h: brick_height, w: brick_width, t: brick_top, l: brick_left });
-							}
-						}, 3000);
-		    		}
-		    		if ($scope.score == 144) {
-		    			gameEnd = true;
-		    			gameWin = true;
-		    		}
-		    	});
-		    }
-
-		    // Move the ball to its new position.
-		    if (!gameEnd) {
-		    	ball_x += ball_dx;
-		    	ball_y += ball_dy;
-		    } 
-
-		    // If the ball has hit the right side, bounce it.
-		    if (ball_x + (ball_size/2) >= canvas.width) {
-		    	ball_dx = -ball_dx;
-		    	ball_x -= 2;
-		    }
-
-		    // If the ball has hit the left side, bounce it.
-		    if (ball_x - (ball_size/2) <= 0) {
-		    	ball_dx = -ball_dx;
-		    	ball_x += 2;
-		    }
-
-		    // If the ball has hit the top, bounce it.
-		    if (ball_y - (ball_size/2) <= 0) { 
-		    	ball_dy = -ball_dy;
-		    	ball_y += 2;
-		    }
-
-		    if (ball_y >= canvas.height - 10 && ball_y < canvas.height) {
-			    gameEnd = true;
+			// Game end function if you've sunk below the canvas
+			if (player_y > 505) {
+				gameEnd = true;
 			}
+
+			// Game end function if you've hit an obstacle
+			// top-left corner, top-right corner, bottom-left corner, bottom-right corner
+			if (ctx.getImageData(player_x - (1 + player_width/2), player_y - (1 + player_height/2), 1, 1).data[0] == 241
+				|| ctx.getImageData(player_x + player_width/2, player_y - (1 + player_height/2), 1, 1).data[0] == 241
+				|| ctx.getImageData(player_x - (1 + player_width/2), player_y + player_height/2, 1, 1).data[0] == 241
+				|| ctx.getImageData(player_x + player_width/2, player_y + player_height/2, 1, 1).data[0] == 241) {
+				gameEnd = true;
+			}
+
+		    // Constantly move the screen view slowly down
+		    if (!gameEnd) {
+		    	// move the player down
+		    	player_y += 0.5;
+
+		    	// move the obstacles down
+		    	// and move the rows left or right
+		    	for (var i = 0; i < obstacles.length; i++) {
+					for (var j = 0; j < obstacles[i].length; j++) {
+						obstacles[i][j].top += 0.5;
+						obstacles[i][j].left += (obstacles[i][j].speed * obstacles[i][j].direction);
+					}
+				}
+		    } 
 
 			return true;
 		};
@@ -289,61 +152,145 @@
 		// Draw everything
 		$scope.draw = function() {
 			// Clear the canvas
-			ctx.fillStyle="#050505";
+			ctx.fillStyle = "#050505";
 			ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-			// TEST
-			ctx.fillStyle="#777";
-			ctx.fillRect(0, canvas.height - 5, canvas.width, canvas.height);
-			// END TEST
-
-			// Draw the paddle
+			// Draw the player
 			ctx.fillStyle = "#f2f2f2";
-			ctx.fillRect(paddle_x - paddle_width/2, paddle_y - paddle_height/2, paddle_width, paddle_height);
+			ctx.fillRect(player_x - player_width/2, player_y - player_height/2, player_width, player_height);
 
-			// Draw the ball
+			// Draw the obstacles
 			ctx.fillStyle = "#f1f1f1";
-			ctx.fillRect(ball_x - ball_size/2, ball_y - ball_size/2, ball_size, ball_size);
-
-			// Drawthe bricks
-			ctx.fillStyle = "#f1f1f1";
-			for (var i = 0; i < $scope.brickArray.length; i++) {
-				ctx.fillRect($scope.brickArray[i].l, $scope.brickArray[i].t, $scope.brickArray[i].w, $scope.brickArray[i].h);
+			for (var i = 0; i < obstacles.length; i++) {
+				// only draw them if they're in view
+				if (obstacles[i][0].top < 550 && obstacles[i][0].top > -50) {
+					for (var j = 0; j < obstacles[i].length; j++) {
+						ctx.fillRect(obstacles[i][j].left, obstacles[i][j].top, obstacles[i][j].size, obstacles[i][j].height);
+					}
+				}
 			}
 		};
 
+		// Build your obstacles
+		$scope.buildObstacles = function() {
+			// build them one row at a time
+			for (var i = 0; i < 3000; i++) {
+				// so randomly pick each of the variables
+				// the pick the vertical height space
+				var currentRow = [];
+				var currentRowSettings = {
+					top: obstacleSettings.top,
+					left: obstacleSettings.left,
+					height: obstacleSettings.height,
+					size: obstacleSettings.size[ Math.floor(Math.random() * obstacleSettings.size.length) ],
+					speed: obstacleSettings.speed[ Math.floor(Math.random() * obstacleSettings.speed.length) ],
+					direction: obstacleSettings.direction[ Math.floor(Math.random() * obstacleSettings.direction.length) ],
+					spacing_horizontal: obstacleSettings.spacing_horizontal[ Math.floor(Math.random() * obstacleSettings.spacing_horizontal.length) ],
+					spacing_vertical: obstacleSettings.spacing_vertical[ Math.floor(Math.random() * obstacleSettings.spacing_vertical.length) ]
+				};
+
+				// place obstacles in the row
+				while (currentRowSettings.left < 10000) {
+					currentRow.push(
+						{
+							top: currentRowSettings.top,
+							left: currentRowSettings.left,
+							height: currentRowSettings.height,
+							size: currentRowSettings.size,
+							speed: currentRowSettings.speed,
+							direction: currentRowSettings.direction,
+							spacing_horizontal: currentRowSettings.spacing_horizontal,
+							spacing_vertical: currentRowSettings.spacing_vertical
+						}
+					);
+					currentRowSettings.left += (currentRowSettings.size + currentRowSettings.spacing_horizontal);
+				}
+
+				// place that row in the array of all obstacles
+				obstacles.push(currentRow);
+
+				// move the top position up for the next row
+				obstacleSettings.top -= (currentRowSettings.height + currentRowSettings.spacing_vertical);
+			}
+
+			console.log(obstacles);
+		}
+
+		// Keep score
+		$scope.updateScore = $interval(function() {
+			if (gameStart && !gameEnd) {
+				$scope.score++;
+			}
+		}, 1000);
+
+		// Move your player on keydown
 		document.onkeydown = function(e) {
 			var theKey = e.keyCode;
 			switch (theKey) {
 				// Controls
 				case 37: // Left
-					left = true;
+					left = true; // Will take priority over the right key
+					break;
+				case 38: // Up
+					up = true; // Will take priority over the down key
+					// Start the game
+					if (!gameStart) {
+						gameStart = true;
+						$scope.mainLoop();
+					}
 					break;
 				case 39: // Right
-					right = true; // Will take priority over the left key
+					right = true;
+					break;
+				case 40: // Down
+					down = true;
 					break;
 			};
 		};
 
+		// Stop moving your player on keyup
 		document.onkeyup = function(e) {
 			var theKey = e.keyCode;
 			switch (theKey) {
 				// Controls
 				case 37: // Left
-					left = false;
+					left = false; // Will take priority over the right key
+					break;
+				case 38: // Up
+					up = false; // Will take priority over the down key
 					break;
 				case 39: // Right
-					right = false; // Will take priority over the left key
+					right = false;
+					break;
+				case 40: // Down
+					down = false;
 					break;
 			};
 		};
 
-		// Start the game
-		$scope.mainLoop();
+
+		///////////////////////////////////////////////
+		/// GAME INIT
+		///////////////////////////////////////////////
+
+		// set up the canvas
+		var canvas = document.getElementById("gameCanvas");
+		var ctx = canvas.getContext("2d");
+
+		// build your obstacles
+		$scope.buildObstacles();
+
+		// draw the first frame
+		$scope.draw();
+
+		// set the opening text that displays on the canvas
+		ctx.fillStyle = "#f1f1f1";
+		ctx.font = "14px zig";
+		ctx.fillText("PRESS THE UP ARROW KEY TO START",33,400);
 
 	};
 
-	gameController.$inject = ['$scope', '$routeParams', '$rootScope', '$location', '$timeout'];
+	gameController.$inject = ['$scope', '$routeParams', '$rootScope', '$location', '$timeout', '$interval'];
 
 	angular.module('CrossyBlock')
 	    .controller('gameController', gameController);
